34e09035cd72ff1db0db25ae2a8847e86bd8e289
Alexander <s.aleks-02@mail.ru>
2025-01-28 01:59:51 +0300

    new logic for bot iterrupting

--- bot.py
+++ bot.py
@@ -10,9 +10,22 @@ dp = Dispatcher()
 dp.message.middleware(LoggingMiddleware())
 setup_handlers(dp)
 
+async def on_shutdown(dp: Dispatcher):
+    await dp.storage.close()
+    await bot.session.close()
+
 async def main():
     print("Бот запущен!")
-    await dp.start_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    try:
+        await dp.start_polling(bot)
+    except asyncio.CancelledError:
+        print("Бот завершает работу...")
+        await on_shutdown(dp)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот завершает работу...")
+        asyncio.run(on_shutdown(dp))
